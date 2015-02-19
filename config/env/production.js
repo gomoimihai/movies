a15ebf06fdde1eeb05753789cbd8a1373a9c44d5
Mihai Bogdan Gomoi <gomoimihai@gmail.com>
2015-02-19 11:57:00 +0200

    Changed Db connection credentials

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: 'mongodb://' + 'lethicus:101spqr@ds037551.mongolab.com:37551' + '/movies',
   /**
    * Database options that will be passed directly to mongoose.connect
    * Below are some examples.
